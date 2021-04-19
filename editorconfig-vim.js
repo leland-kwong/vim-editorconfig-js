131b22df0bf6b44a14b195cfcc1cb63374332a02
Leland Kwong <leland.kwong@gmail.com>
2021-04-18 23:28:15 -0700

    initial wip

---
+++ editorconfig-vim.js
@@ -0,0 +1,16 @@
+async function run() {
+  const args = process.argv
+  const [,,filePath] = args;
+  const editorconfig = require('editorconfig')
+  const promise = editorconfig.parse(filePath);
+
+  promise.then(function onFulfilled(result) {
+    process.stdout.write(JSON.stringify(result) + '\n')
+  }).catch(function onError(err) {
+    const errorResponse = JSON.stringify({
+      err: err.message
+    })
+    process.stdout.write(errorResponse + '\n')
+  });
+}
+run()
