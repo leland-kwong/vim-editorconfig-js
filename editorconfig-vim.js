d4de97dff4cf60edc8d19befb179a29d4b1a1837
Leland Kwong <leland.kwong@gmail.com>
2021-05-02 19:17:02 -0700

    reformat with prettier

--- editorconfig-vim.js
+++ editorconfig-vim.js
@@ -1,16 +1,18 @@
 async function run() {
-  const args = process.argv
-  const [,,filePath] = args;
-  const editorconfig = require('editorconfig')
+  const args = process.argv;
+  const [, , filePath] = args;
+  const editorconfig = require("editorconfig");
   const promise = editorconfig.parse(filePath);
 
-  promise.then(function onFulfilled(result) {
-    process.stdout.write(JSON.stringify(result) + '\n')
-  }).catch(function onError(err) {
-    const errorResponse = JSON.stringify({
-      err: err.message
+  promise
+    .then(function onFulfilled(result) {
+      process.stdout.write(JSON.stringify(result) + "\n");
     })
-    process.stdout.write(errorResponse + '\n')
-  });
+    .catch(function onError(err) {
+      const errorResponse = JSON.stringify({
+        err: err.message,
+      });
+      process.stdout.write(errorResponse + "\n");
+    });
 }
-run()
+run();
