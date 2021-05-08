0e29c6534d9f96b59f9c461943b3902e1a29f357
Leland Kwong <leland.kwong@gmail.com>
2021-05-07 20:53:34 -0700

    add prettierrc
    * also adding files that got autoformatted by prettier

--- editorconfig-vim.js
+++ editorconfig-vim.js
@@ -1,18 +1,18 @@
 async function run() {
-  const args = process.argv;
-  const [, , filePath] = args;
-  const editorconfig = require("editorconfig");
-  const promise = editorconfig.parse(filePath);
+  const args = process.argv
+  const [, , filePath] = args
+  const editorconfig = require('editorconfig')
+  const promise = editorconfig.parse(filePath)
 
   promise
     .then(function onFulfilled(result) {
-      process.stdout.write(JSON.stringify(result) + "\n");
+      process.stdout.write(JSON.stringify(result) + '\n')
     })
     .catch(function onError(err) {
       const errorResponse = JSON.stringify({
-        err: err.message,
-      });
-      process.stdout.write(errorResponse + "\n");
-    });
+        err: err.message
+      })
+      process.stdout.write(errorResponse + '\n')
+    })
 }
-run();
+run()
