a4c2c90780556119b6e499f0ca471461b864b212
Leland Kwong <leland.kwong@gmail.com>
2021-05-09 09:45:04 -0700

    refactor js code to use async/await

--- editorconfig-vim.js
+++ editorconfig-vim.js
@@ -1,15 +1,14 @@
+const editorconfig = require('editorconfig')
+
 async function run() {
   const args = process.argv
   const [, , filePath] = args
-  const editorconfig = require('editorconfig')
-  const promise = editorconfig.parse(filePath)
 
-  promise
-    .then(function onFulfilled(result) {
-      process.stdout.write(JSON.stringify(result) + '\n')
-    })
-    .catch(function onError(err) {
-      process.stderr.write(err + '\n')
-    })
+  try {
+    const result = await editorconfig.parse(filePath)
+    process.stdout.write(JSON.stringify(result) + '\n')
+  } catch (err) {
+    process.stderr.write(err + '\n')
+  }
 }
 run()
